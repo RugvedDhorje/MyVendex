85b073e666739da0f25d11f9df70f9b8bd88ee74
RugvedDhorje <rdhorje6@gmail.com>
2024-09-14 00:15:49 +0530

    card route added

---
+++ app/card/page.tsx
@@ -0,0 +1,38 @@
+import Card from "@/components/Card/Card";
+
+const cardImages = [
+  "https://images.unsplash.com/photo-1482938289607-e9573fc25ebb?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1433086966358-54859d0ed716?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1509316975850-ff9c5deb0cd9?q=80&w=3072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1518495973542-4542c06a5843?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1513836279014-a89f7a76ae86?q=80&w=3088&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1504893524553-b855bce32c67?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1500375592092-40eb2168fd21?q=80&w=3088&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1498462440456-0dba182e775b?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1505118380757-91f5f5632de0?q=80&w=2902&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1491378630646-3440efa57c3b?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+];
+
+const cards = cardImages.map((image) => ({
+  image,
+  title: "Rudram Market Development",
+  subtitle: "Rudram Market Development",
+}));
+export default function Cards() {
+  return (
+    <div className="w-screen min-h-screen bg-white">
+        <h1 style={{color:"yellow"}} className="pt-5 text-[100px] text-center">Vendors</h1>
+        <div className=" px-5 flex flex-wrap gap-5 justify-center py-4">
+      {cards.map((card, index) => (
+        <div key={index} className="min-w-[20vw]">
+          <Card
+            image={card.image}
+            title={card.title}
+            subtitle={card.subtitle}
+          />
+        </div>
+      ))}
+    </div>
+    </div>
+  );
+}
